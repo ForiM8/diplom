79640ae207fb259e8a7c85b2d2326b8c36651b4f
ForiM8 <nik.maltsev200470@gmail.com>
2025-04-26 18:55:37 +0700

    feat: типизация для юзера

---
+++ src/types/User.types.ts
@@ -0,0 +1,30 @@
+export type CreateUser = {
+    email: string
+    name: string
+    password: string
+    confirm_password: string
+}
+
+export type User = {
+    city_id: number
+    createdAt: string
+    email: string
+    id: number
+    name: string
+    password_hash: string
+    phone: number
+    role: number
+    status: number
+    telegram: string
+    updatedAt: string
+    address: string
+    street: string
+    house: string
+    flat: string
+    zip_code: string
+}
+
+export type LoginUser = {
+    email: string
+    password: string
+}
